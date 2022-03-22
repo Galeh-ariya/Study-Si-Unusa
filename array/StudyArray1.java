a70311f38de589e9aff8f532aa072a03f3702e58
Galeh Ariya Irwana <galehariya8@gmail.com>
2022-03-22 23:11:57 +0700

    File Source Code Array 20/03/22

--- array/StudyArray1.java
+++ array/StudyArray1.java
@@ -1,16 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package Pertemuan4;
 
 /**
- *
  * @author Nanda D. Cahyo
  */
-public class ArrayTigaDimensi {
+public class StudyArray1 {
     public static void main(String[] args) {
         int[] satuD = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12};
         
